25bcf9aec4c7efb29f2f69cd15d8c6f74a781f86
sunkenquest <sunkenquest@gmail.com>
2024-10-02 17:53:05 +0800

    add db connection

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using practice_dotnet.Models.Entities;
+
+namespace practice_dotnet.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions options) : base(options)
+		{
+		}
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
+using System;
+using practice_dotnet.Data;
+using Pomelo.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+	options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
+	new MySqlServerVersion(new Version(8, 0, 39))));
 
 var app = builder.Build();
 
