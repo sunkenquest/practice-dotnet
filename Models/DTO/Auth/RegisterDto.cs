916dc7549b136e1fe47dd643eb2e3fb6b003ddda
m_.kco <83852126+sunkenquest@users.noreply.github.com>
2024-10-04 17:17:02 +0800

    chore: used the login swagger, wip add token (#5)

---
+++ Models/DTO/Auth/RegisterDto.cs
@@ -0,0 +1,8 @@
+﻿namespace practice_dotnet.Models.DTO.Auth
+{
+    public class RegisterDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Repository/Services/AuthRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using practice_dotnet.Models;
+using practice_dotnet.Repository.Interface;
+
+namespace practice_dotnet.Repository.Services
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AuthRepository(SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public async Task<SignInResult> Login(LoginDto model)
+        {
+            return await _signInManager.PasswordSignInAsync(model.Email, model.Password, false, lockoutOnFailure: false);
+
+        }
+    }
+}
