509d248cef18b8f455e9759e76aebacc15ce11da
sunkenquest <sunkenquest@gmail.com>
2024-10-07 13:22:45 +0800

    feat: minimal design for login and register

---
+++ Repository/Services/EmailRepository.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Mail;
+using practice_dotnet.Repository.Interface;
+
+namespace practice_dotnet.Repository.Services
+{
+    public class EmailRepository : IEmailRepository
+    {
+        private readonly SmtpClient _smtpClient;
+
+        public EmailRepository(SmtpClient smtpClient)
+        {
+            _smtpClient = smtpClient;
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string message)
+        {
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress("your-email@example.com"),
+                Subject = subject,
+                Body = message,
+                IsBodyHtml = true,
+            };
+            mailMessage.To.Add(email);
+
+            await _smtpClient.SendMailAsync(mailMessage);
+        }
+    }
+}
