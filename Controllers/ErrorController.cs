ac3a5d47c1852bbe7a386269b952edd8088ee4f9
sunkenquest <sunkenquest@gmail.com>
2024-10-08 14:33:24 +0800

    fix: compile error in swagger

--- Controllers/ErrorController.cs
+++ Controllers/ErrorController.cs
@@ -5,18 +5,21 @@ namespace practice_dotnet.Controllers
     public class ErrorController : Controller
     {
         [Route("Error/404")]
+        [HttpGet]
         public IActionResult NotFoundPage()
         {
             return View("NotFound");
         }
 
         [Route("Error/401")]
+        [HttpGet]
         public IActionResult UnauthorizedPage()
         {
             return View("Unauthorized");
         }
 
         [Route("Error/500")]
+        [HttpGet]
         public IActionResult GenericErrorPage()
         {
             return View("GenericError");
